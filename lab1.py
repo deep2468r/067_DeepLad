14157db7606adcb100d2df0159b68c5cd429320b
Deep Lad <deep2468r@gmail.com>
2021-07-09 16:13:35 +0530

    Module added

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-import nlkt, scipy, numpy, matplotlib, pandas
+import nltk, scipy, numpy, matplotlib, pandas
 # nlkt has Natural Language Tool Kit to process Text Data
 # SciPy is used to provide scientific  calculations necessary for machine applications
 # NumPy provide methods for Numerical calculations / Linear Algebra
