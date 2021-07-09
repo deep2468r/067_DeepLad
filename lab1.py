b7951c2545117f52a72223d0cdd7b87fec334d8b
Deep Lad <deep2468r@gmail.com>
2021-07-09 16:05:28 +0530

    Some comments added

--- lab1.py
+++ lab1.py
@@ -1,1 +1,5 @@
 import nlkt, scipy, numpy, matplotlib, pandas
+# nlkt has Natural Language Tool Kit to process Text Data
+# SciPy is used to provide scientific  calculations necessary for machine applications
+# NumPy provide methods for Numerical calculations / Linear Algebra
+# MatPlotLib is for graph representation
